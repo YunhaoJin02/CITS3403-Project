8c656419d66377e01cb0662116508e3746f6ddb9
yj <yjin4782@uni.sydney.edu.au>
2024-05-03 01:28:15 +0800

    Event updated

--- Desktop/uwa/units/cits3403/project/event_detail.js
+++ Desktop/uwa/units/cits3403/project/event_detail.js
@@ -2,13 +2,18 @@
 function displayEventDetails(event) {
     // Assuming you have placeholders for these details in your event_detail.html
     document.getElementById('event-title').textContent = event.title;
-    document.getElementById('event-date').textContent = event.date;
-    document.getElementById('event-location').textContent = event.location;
-    document.getElementById('event-description').textContent = event.description;
+    document.getElementById('event-date').textContent = `Date: ${event.date}`;
+    document.getElementById('event-location').textContent = `Location: ${event.location}`;
+    document.getElementById('event-description').textContent = event.description[0].subheader;
     document.getElementById('img1').src = event.img1;
     document.getElementById('img2').src = event.img2;
-    document.getElementById('event-price').src = `Price: ${event.price ? `$${event.price}` : 'Free'}`;
+    document.getElementById('img3').src = event.img3;
+    document.getElementById('img4').src = event.img4;
 
+    document.getElementById('event-price').src = `Price: ${event.price}`;
+
+    const priceInfo = event.price === 0 ? 'Free' : `$${event.price}`;
+    document.getElementById('event-price').textContent = `Ticket fee: ${priceInfo}`;
   }
   
   document.addEventListener('DOMContentLoaded', () => {
