71ca8807dfaca5cc70555ee1de42a0f4ac6b7af8
xinchengli1112 <lixincheng57@163.com>
2024-05-13 16:34:26 +0800

    13/05 4:34pm

---
+++ Pets forum/Pets forum/Web project/app/forms.py
@@ -0,0 +1,46 @@
+import wtforms
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, IntegerField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, InputRequired
+from app.models import UserModel
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class StaffLoginForm(FlaskForm):
+    staff_username = StringField('Staff Username', validators=[DataRequired()])
+    staff_password = PasswordField('Staff Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+
+# User Registration form 
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = UserModel.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = UserModel.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+        
+
+class QuestionForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+
+class AnswerForm(wtforms.Form):
+    content = TextAreaField('Content', validators=[DataRequired()])
+    question_id = IntegerField('Question ID', validators=[InputRequired()])
